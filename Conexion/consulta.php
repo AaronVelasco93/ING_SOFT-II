2e4bdb584ad88f4a91039894ab342c31487a5565
AaronVelasco93 <aaronvelasco1993@outlook.com>
2021-06-17 18:13:46 -0500

    consulta segunda parte[C

--- Conexion/consulta.php
+++ Conexion/consulta.php
@@ -12,7 +12,39 @@ $resultado = $conexion->query($consulta_sql);
 
 $count = mysqli_num_rows($resultado);
 
+//creacion de unas columas con ayuda de html
 
+echo "<table border='2' >";
+echo "<tr>";
+echo    "<th>ID de Trabajador</th>";
+echo    "<th>User de Acceso al Sistema</th>";
+echo    "<th>Nombre de Usuario</th>";
+echo    "<th>Correo electronico</th>";
+
+echo "</tr>";
+
+if($count>0){
+    while ($row = mysqli_fetch_assoc($resultado) ){
+echo        "<tr>";
+  echo  "<td>".$row['id']."</td>";
+  echo  "<td>".$row['user']."</td>";
+
+
+echo "</tr>";
+
+
+
+
+    }
+
+
+}else{
+
+echo "<h1>Sin datos en la DB</h1>";
+
+}
 
 
 ?>
+
+</table>
