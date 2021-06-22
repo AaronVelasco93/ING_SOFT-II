ba71e0be3fcbfbd745abe11a3f081cd02861792d
AaronVelasco93 <aaronvelasco1993@outlook.com>
2021-06-22 17:58:49 -0500

    ejemplo de delete

---
+++ Clase2/deleteUsuario.php
@@ -0,0 +1,17 @@
+<?php
+    require "conexion.php";
+    mysqli_set_charset($conexion, 'utf8');
+
+    $registroEliminado=$_POST['no_cuenta'];
+
+
+
+    $consulta="DELETE FROM PERSONA WHERE no_cuenta =";
+
+    mysqli_query($conexion, $consulta . $registroEliminado);
+    
+    mysqli_close($conexion);
+    echo " <h2>Registro eliminado</h2>";
+
+
+?>
